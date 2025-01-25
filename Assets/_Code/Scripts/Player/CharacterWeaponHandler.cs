31da7ae683a091ddd8ad88e8cfc3799ae9fbce3d
Kiko-BZ <kikoalfaro2@gmail.com>
2025-01-25 23:51:21 +0100

    UPDATE - Character controls working nice

--- Assets/_Code/Scripts/Player/CharacterWeaponHandler.cs
+++ Assets/_Code/Scripts/Player/CharacterWeaponHandler.cs
@@ -5,9 +5,9 @@ using UnityEngine.InputSystem;
 
 public class CharacterWeaponHandler : MonoBehaviour
 {
-    [SerializeField] private PlayerInput _playerInput;
-    [SerializeField] private int _currentWeaponIndex = 0;
-    [SerializeField] private int _totalWeaponsAmount = 3;
+    private PlayerInput _playerInput;
+    private int _currentWeaponIndex = 0;
+    private int _totalWeaponsAmount = 3;
 
     public Animator CurrentWeaponAnimator => Current.Animator;
     [SerializeField] private Weapon[] _weapons;
@@ -15,12 +15,17 @@ public class CharacterWeaponHandler : MonoBehaviour
     public Weapon Current => _weapons[_currentWeaponIndex];
     public UnityAction<Weapon> OnWeaponChanged;
 
+    private bool _isChangingWeapon;
+
 
     private void Awake()
     {
+        _playerInput = GetComponentInParent<PlayerInput>();
         _playerInput.actions["Attack"].performed += OnAttack;
         _playerInput.actions["Previous"].performed += OnPrevious;
         _playerInput.actions["Next"].performed += OnNext;
+
+        _totalWeaponsAmount = _weapons.Length;
     }
 
     private void OnAttack(InputAction.CallbackContext context)
@@ -30,23 +35,28 @@ public class CharacterWeaponHandler : MonoBehaviour
 
     private async void OnPrevious(InputAction.CallbackContext context)
     {
-        Current.Hide();
-        await Task.Delay(1000);
-        Current.Animator.gameObject.SetActive(false);
-
-        _currentWeaponIndex = (_currentWeaponIndex - 1 + _totalWeaponsAmount) % _totalWeaponsAmount;
-        Current.Show();
-        OnWeaponChanged(Current);
+        await ChangeWeaponIndex(-1);
     }
 
     private async void OnNext(InputAction.CallbackContext context)
     {
+        await ChangeWeaponIndex(1);
+    }
+
+    private async Task ChangeWeaponIndex(int direction)
+    {
+        if (_isChangingWeapon) return;
+
+        _isChangingWeapon = true;
         Current.Hide();
         await Task.Delay(1000);
         Current.Animator.gameObject.SetActive(false);
-        
-        _currentWeaponIndex = (_currentWeaponIndex + 1 + _totalWeaponsAmount) % _totalWeaponsAmount;
+
+        _currentWeaponIndex = (_currentWeaponIndex + direction + _totalWeaponsAmount) % _totalWeaponsAmount;
+
         Current.Show();
         OnWeaponChanged(Current);
+        _isChangingWeapon = false;
     }
+
 }
