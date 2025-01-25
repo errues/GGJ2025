bfce468e764630dc794f282868a5cf226d16c43e
errues <errues@gmail.com>
2025-01-25 21:30:33 +0100

    Aparición de charcos

--- Assets/_Code/Scripts/Interactables/GarbagePiece.cs
+++ Assets/_Code/Scripts/Interactables/GarbagePiece.cs
@@ -1,52 +1,21 @@
 using UnityEngine;
 
-public class GarbagePiece : MonoBehaviour, IInteractable {
-    [SerializeField] private Transform modelParent;
+public class GarbagePiece : Dirt {
+    [Header("Hygiene Values")]
     [SerializeField] private int hygieneValue = 1;
 
-    [Header("Sounds")]
-    [SerializeField] private ScriptableSound appearSound;
-    [SerializeField] private ScriptableSound cleanSound;
+    public override void CancelInteract() { }
 
-    private Animator animator;
-    private GarbageGenerator garbageGenerator;
-    private AudioSource audioSource;
-
-    private int randomModelIndex;
-    private bool active;
-
-    private void Awake() {
-        animator = GetComponent<Animator>();
-        garbageGenerator = GetComponentInParent<GarbageGenerator>();
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    public void CancelInteract() { }
-
-    public bool CanInteract() {
+    public override bool CanInteract() {
         return active;
     }
 
-    public void Interact() {
-        Disappear();
-    }
-
-    public void Appear() {
-        active = true;
-        randomModelIndex = Random.Range(0, modelParent.childCount);
-        modelParent.GetChild(randomModelIndex).gameObject.SetActive(true);
-        animator.SetTrigger("Appear");
-        modelParent.Rotate(0, Random.Range(0f, 360f), 0);
-        audioSource.PlayOneShot(appearSound.GetRandomClip());
-        MessageBus.Instance.Notify("ReduceHygiene", hygieneValue);
-    }
-
-    public void Disappear() {
-        active = false;
-        modelParent.GetChild(randomModelIndex).gameObject.SetActive(false);
-        garbageGenerator.SetGarbagePieceInactive(this);
-        audioSource.PlayOneShot(cleanSound.GetRandomClip());
+    protected override void AddHygiene() {
         MessageBus.Instance.Notify("AddHygiene", hygieneValue);
+    }
+
+    protected override void ReduceHygiene() {
+        MessageBus.Instance.Notify("ReduceHygiene", hygieneValue);
     }
 
     private void OnDrawGizmos() {
