cd70c89a89336842b7c41d7fa887b1d40f8da4a2
Kiko-BZ <kikoalfaro2@gmail.com>
2025-01-25 08:57:28 +0100

    ADD - Dirty shader and some basic scripts

---
+++ Assets/Kiko/Scripts/MessageBus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageBus
+{
+    private static MessageBus _instance;
+    public static MessageBus Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new MessageBus();
+            return _instance;
+        }
+    }
+
+    private Dictionary<string, Action<object>> _eventTable = new Dictionary<string, Action<object>>();
+
+    public void Subscribe(string tag, Action<object> callback)
+    {
+        if (!_eventTable.ContainsKey(tag))
+            _eventTable.Add(tag, null);
+
+        _eventTable[tag] += callback;
+    }
+
+    public void Unsubscribe(string tag, Action<object> callback)
+    {
+        if (_eventTable.ContainsKey(tag))
+            _eventTable[tag] -= callback;
+    }
+
+    public void Notify(string tag, object arg = null)
+    {
+        if (_eventTable.ContainsKey(tag) && _eventTable[tag] != null)
+            _eventTable[tag].Invoke(arg);
+        else Debug.Log($"<color=red>OJO! El evento notificado con tag \"{tag}\" no tiene suscriptores. Revisa la tag o suscríbete.</color>");
+    }
+}
---
+++ Assets/Kiko/Scripts/StateMachine.cs
@@ -0,0 +1,28 @@
+﻿public class StateMachine
+{
+    private State _previousState;
+    private State _currentState;
+
+    public State CurrentState { get => _currentState; }
+
+    public void Initialize(State defaultState)
+    {
+        _currentState = defaultState;
+        _previousState = _currentState;
+
+        defaultState.OnEnter();
+    }
+
+    public void ChangeState(State newState)
+    {
+        _currentState.OnExit();
+        _previousState = _currentState;
+        _currentState = newState;
+        _currentState.OnEnter();
+    }
+
+    public void BackToPreviousState()
+    {
+        ChangeState(_previousState);
+    }
+}
