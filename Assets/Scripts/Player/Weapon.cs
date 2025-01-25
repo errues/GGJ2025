7fb542cee552298445ee70eaf915ce82d301301b
Kiko-BZ <kikoalfaro2@gmail.com>
2025-01-25 10:22:05 +0100

    ADD - Weapon use and switch

---
+++ Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,20 @@
+﻿public class Weapon
+{
+    public WeaponModel Model;
+}
+
+
+
+
+//public class PickeableWeapon : IInteractable
+//{
+//    public bool CanInteract()
+//    {
+//        throw new System.NotImplementedException();
+//    }
+
+//    public void Interact()
+//    {
+//        throw new System.NotImplementedException();
+//    }
+//}
