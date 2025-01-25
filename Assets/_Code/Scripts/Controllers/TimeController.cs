38f7b0acd5d8ac1ed26239456d637c1470184457
errues <errues@gmail.com>
2025-01-25 19:46:24 +0100

    Temporizador y mensajes de Game Over

---
+++ Assets/_Code/Scripts/Controllers/TimeController.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour {
+    private const float RealtimeDuration = 172800;
+
+    [SerializeField, Range(10, 600)] private float gameDuration = 300;
+    [SerializeField] private TextMeshProUGUI text;
+
+    private float remainingTime;
+
+    private void Awake() {
+        remainingTime = gameDuration;
+    }
+
+    void Update() {
+        remainingTime = Mathf.Clamp(remainingTime - Time.deltaTime, 0, gameDuration);
+
+        UpdateTimerText();
+
+        if (remainingTime <= 0) {
+            MessageBus.Instance.Notify("TimeUp");
+        }
+    }
+
+    private void UpdateTimerText() {
+        float hypotheticalTime = remainingTime * (RealtimeDuration / gameDuration);
+        int hours = (int)hypotheticalTime / 3600;
+        int minutes = ((int)hypotheticalTime - hours * 3600) / 60;
+        text.text = (hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes;
+    }
+}
