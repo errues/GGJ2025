4cce60891612e46a1696def7294930d0cca42b94
errues <errues@gmail.com>
2025-01-26 04:26:11 +0100

    Perfiles de pospro y cambio suavizado

--- Assets/_Code/Scripts/Controllers/HygieneController.cs
+++ Assets/_Code/Scripts/Controllers/HygieneController.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class HygieneController : MonoBehaviour {
     [SerializeField] private Image hygieneBar;
     [SerializeField] private int maxHygiene = 100;
 
+    [Header("Post-Processing")]
+    [SerializeField, Range(0, 1)] private float postprocessingChangeSpeed = .2f;
+    [SerializeField] private Volume cleanVolume;
+    [SerializeField] private Volume midVolume;
+    [SerializeField] private Volume dirtyVolume;
+
     private int currentHygiene;
+    private float cleanWeightTarget;
+    private float midWeightTarget;
+    private float dirtyWeightTarget;
 
     private void Awake() {
         currentHygiene = maxHygiene;
@@ -16,21 +26,32 @@ public class HygieneController : MonoBehaviour {
         MessageBus.Instance.Subscribe("ReduceHygiene", ReduceHygiene);
     }
 
+    private void Update() {
+        cleanVolume.weight = Mathf.MoveTowards(cleanVolume.weight, cleanWeightTarget, postprocessingChangeSpeed * Time.deltaTime);
+        midVolume.weight = Mathf.MoveTowards(midVolume.weight, midWeightTarget, postprocessingChangeSpeed * Time.deltaTime);
+        dirtyVolume.weight = Mathf.MoveTowards(dirtyVolume.weight, dirtyWeightTarget, postprocessingChangeSpeed * Time.deltaTime);
+    }
+
     private void AddHygiene(object value) {
         currentHygiene = Mathf.Clamp(currentHygiene + (int)value, 0, maxHygiene);
-        UpdateBar();
+        UpdateVisuals();
     }
 
     private void ReduceHygiene(object value) {
         currentHygiene = Mathf.Clamp(currentHygiene - (int)value, 0, maxHygiene);
-        UpdateBar();
+        UpdateVisuals();
 
         if (currentHygiene == 0) {
             MessageBus.Instance.Notify("GameOver");
         }
     }
 
-    private void UpdateBar() {
-        hygieneBar.fillAmount = 1f * currentHygiene / maxHygiene;
+    private void UpdateVisuals() {
+        float normalizedHygiene = 1f * currentHygiene / maxHygiene;
+        hygieneBar.fillAmount = normalizedHygiene;
+
+        cleanWeightTarget = Mathf.Lerp(0, 1, (normalizedHygiene - .5f) / .33f);
+        midWeightTarget = .5f - (Mathf.Abs(.5f - normalizedHygiene) - 0.166f) / .33f;
+        dirtyWeightTarget = 1 - Mathf.Lerp(0, 1, (normalizedHygiene - .166f) / .33f);
     }
 }
