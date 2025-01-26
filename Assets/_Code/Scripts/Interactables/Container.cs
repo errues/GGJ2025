7a0eb93df70002ed07bfcc60c4d7bd875e5eff17
errues <errues@gmail.com>
2025-01-26 16:02:44 +0100

    Tirar la basura al container

--- Assets/_Code/Scripts/Interactables/Container.cs
+++ Assets/_Code/Scripts/Interactables/Container.cs
@@ -1,5 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
 public class Container : FixInteractable {
+    [SerializeField] private GameObject garbageBin;
+
+    private Coroutine garbageCoroutine;
+
+
     protected override void InteractionAction() {
-        GarbageManager.Instance.EmptyGarbage();
+        if (GarbageManager.Instance.CurrentGarbageAmount > 0) {
+            GarbageManager.Instance.EmptyGarbage();
+
+            if (garbageCoroutine != null) {
+                StopCoroutine(garbageCoroutine);
+            }
+            garbageCoroutine = StartCoroutine(GarbageSequence());
+        }
+    }
+
+    private IEnumerator GarbageSequence() {
+        garbageBin.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        garbageBin.SetActive(false);
     }
 }
