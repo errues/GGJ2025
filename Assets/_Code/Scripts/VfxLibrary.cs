17b68b6f744a6f5a8286768048acdeaaf1fbbb3f
Kiko-BZ <kikoalfaro2@gmail.com>
2025-01-26 11:14:28 +0100

    FX Scripts recovered

---
+++ Assets/_Code/Scripts/VfxLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "VfxLibrary", menuName = "ScriptableObjects/VfxLibrary", order = 1)]
+public class VfxLibrary : ScriptableObject
+{
+    [SerializeField]
+    public VfxPair[] VfxCollection;
+
+
+    public GameObject GetVfxById(string id)
+    {
+        return VfxCollection.First(pair => pair.Id == id).Vfx;
+    }
+
+    [Serializable]
+    public struct VfxPair
+    {
+        public string Id;
+        public GameObject Vfx;
+    }
+
+}
