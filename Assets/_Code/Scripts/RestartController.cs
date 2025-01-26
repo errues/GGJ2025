92f427f05eb0524e1774a4ece9883b970614903e
Jony-BZ <jony.salazar@bravezebra.es>
2025-01-26 17:52:56 +0100

    Fixed Back to main menu from win/loose scenes after 5 seconds

--- Assets/_Code/Scripts/RestartController.cs
+++ Assets/_Code/Scripts/RestartController.cs
@@ -1,14 +1,32 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RestartController : MonoBehaviour
 {
+    private bool mCanSkip;
+    private void Start()
+    {
+        mCanSkip = false;
+
+        StartCoroutine(_EnableSkip());
+
+    }
+
+    private IEnumerator _EnableSkip()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        mCanSkip = true;
+    }
     void Update()
     {
-        // Detecta cualquier tecla en el teclado o botón en el gamepad.
-        if (Input.anyKeyDown)
+        if(mCanSkip)
         {
-            LoadScene();
+            // Detecta cualquier tecla en el teclado o botón en el gamepad.
+            if (Input.anyKeyDown)
+            {
+                LoadScene();
+            }
         }
     }
 
