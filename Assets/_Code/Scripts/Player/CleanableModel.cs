f187f3931c9786c1e7517425b6d362579af71e7d
errues <errues@gmail.com>
2025-01-26 16:52:35 +0100

    Ajuste de tiempos de suckedad de jammers

--- Assets/_Code/Scripts/Player/CleanableModel.cs
+++ Assets/_Code/Scripts/Player/CleanableModel.cs
@@ -2,26 +2,22 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CleanableModel", menuName = "ScriptableObjects/CleanableModel", order = 1)]
-public class CleanableModel : ScriptableObject, IEquatable<CleanableModel>
-{
+public class CleanableModel : ScriptableObject, IEquatable<CleanableModel> {
     public string Name;
-    public float SecondsBetweenDirtUpgrades = 3;
+    public Vector2 RandomDirtTimeLimits;
     public int MaxDirtLevel = 2;
 
-    public override bool Equals(object obj)
-    {
+    public override bool Equals(object obj) {
         return Equals(obj as CleanableModel);
     }
 
-    public bool Equals(CleanableModel other)
-    {
+    public bool Equals(CleanableModel other) {
         return other != null &&
                base.Equals(other) &&
                Name == other.Name;
     }
 
-    public override int GetHashCode()
-    {
+    public override int GetHashCode() {
         return HashCode.Combine(base.GetHashCode(), Name);
     }
 }
